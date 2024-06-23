3b60bd3e8b5d2fbc40e09d51accba04e091b57c6
Pedro Maironi Toribio <pedromaironi@gmail.com>
2024-06-23 11:51:13 -0700

    Travis ci

---
+++ travis/deploy.sh
@@ -0,0 +1,10 @@
+# Desplegar BFF en Google Cloud
+echo "Desplegando Frontend en S3 Aws..."
+
+# Autenticación en AWS
+
+# Desplegar usando Terraform y kubectl
+cd..
+cd terraform
+terraform init
+terraform apply -auto-approve
