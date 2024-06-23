4494205056f40bed69713794fbff57fb03a0ec40
Pedro Maironi Toribio <pedromaironi@gmail.com>
2024-06-23 16:11:24 -0700

    Commit

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,6 @@ set -e
 
 echo "Desplegando FRONT en aws"
 
-# Decodificar la variable de entorno GCLOUD_SERVICE_KEY de base64 a un archivo temporal
-echo "$GCLOUD_SERVICE_KEY" | base64 --decode > /tmp/gcloud-service-key.json
-
-# Autenticación en Google Cloud
-gcloud auth activate-service-account --key-file=/tmp/gcloud-service-key.json
-
-# Configurar el proyecto de Google Cloud
-gcloud config set project "$GCLOUD_PROJECT_ID"
-
 # Desplegar usando Terraform
 cd terraform
 terraform init
