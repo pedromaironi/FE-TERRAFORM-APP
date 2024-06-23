9cf9e193a5512acb09b0a66eaf7973ddb1f342ad
Pedro Maironi Toribio <pedromaironi@gmail.com>
2024-06-23 14:21:25 -0700

    creds

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 echo "Desplegando Frontend en S3 Aws..."
 
 # Autenticación en AWS
+echo $GCLOUD_SERVICE_KEY | base64 --decode > /tmp/gcloud-service-key.json
+gcloud auth activate-service-account --key-file=/tmp/gcloud-service-key.json
+gcloud config set project $GCLOUD_PROJECT_ID
 
 # Desplegar usando Terraform y kubectl
 cd terraform
