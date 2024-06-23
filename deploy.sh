17a1bfd484ca98708a42ae464da340e25c869936
Pedro Maironi Toribio <pedromaironi@gmail.com>
2024-06-23 15:36:27 -0700

    Commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
-# Desplegar BFF en Google Cloud
-echo "Desplegando Frontend en S3 Aws..."
+set -e
 
-# Autenticación en AWS
-gcloud auth activate-service-account --key-file <(echo "$GCLOUD_SERVICE_KEY")
+echo "Desplegando BFF en Google Cloud"
+
+# Decodificar la variable de entorno GCLOUD_SERVICE_KEY de base64 a un archivo temporal
+echo "$GCLOUD_SERVICE_KEY" | base64 --decode > /tmp/gcloud-service-key.json
+
+# Autenticación en Google Cloud
+gcloud auth activate-service-account --key-file=/tmp/gcloud-service-key.json
+
+# Configurar el proyecto de Google Cloud
 gcloud config set project "$GCLOUD_PROJECT_ID"
 
-# Desplegar usando Terraform y kubectl
+# Desplegar usando Terraform
 cd terraform
 terraform init
 terraform apply -auto-approve
